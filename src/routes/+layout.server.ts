81ee35e9a193ac03f92ee497a83444da8b35e086
awera <aw3ra@proton.me>
2024-02-04 16:12:14 +1100

    Finally

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/__layout.server.ts
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-  return {
-    session: await locals.getSession(),
-  };
-};
