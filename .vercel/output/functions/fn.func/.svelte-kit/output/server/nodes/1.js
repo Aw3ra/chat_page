b62f6495e792e83bdeb64b2f55d7b3be38b06424
awera <aw3ra@proton.me>
2024-02-11 16:32:24 +1100

    test

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.prONbYTG.js","_app/immutable/chunks/scheduler.BnSwxYe-.js","_app/immutable/chunks/index.ikIDff96.js","_app/immutable/chunks/entry.5E1Jt8en.js","_app/immutable/chunks/index.Ua9e-3Rz.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.3oW0242T.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.5E1Jt8en.js";export{t as start};
